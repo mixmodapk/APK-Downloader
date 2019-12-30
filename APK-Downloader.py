e05abd40bbc532a87a80d0cd890ea0d543e50a48
Hamidreza Moradi <realhamidrezamoradi@gmail.com>
2019-12-30 09:35:01 +0330

    Add 6 other sites and changes in user interface

--- APK-Downloader.py
+++ APK-Downloader.py
@@ -1,79 +1,138 @@
-# Coded by: HamidrezaMoradi
+# Coded by: Hamidreza Moradi
 # www.github.com/hamidrezamoradi 
 
 import requests
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
-print('''\033[1;32m
+################# colors #################
+BLUE = '\033[1;34m'
+GREEN = '\033[1;32m'
+YELLOW = '\033[1;33m'
+RED = '\033[1;31m'
+
+RED_BG = '\033[1;77m\033[41m'
+
+CLOSE_COLOR = '\033[m'
+##########################################
+
+print(f'''{GREEN}
      ___    ____  __ __ ____                      __                __         
     /   |  / __ \/ //_// __ \____ _      ______  / /___  ____ _____/ /__  _____
    / /| | / /_/ / ,<  / / / / __ \ | /| / / __ \/ / __ \/ __ `/ __  / _ \/ ___/
   / ___ |/ ____/ /| |/ /_/ / /_/ / |/ |/ / / / / / /_/ / /_/ / /_/ /  __/ /    
  /_/  |_/_/   /_/ |_/_____/\____/|__/|__/_/ /_/_/\____/\__,_/\__,_/\___/_/     
 
- \033[1;77m\033[41m      Google Play  Downloader  v1.5,  Author:  @HamidrezaMoradi  (Github)     \033[m\n
+ {RED_BG}      Google Play  Downloader  v2.0,  Author:  @HamidrezaMoradi  (Github)     {CLOSE_COLOR}\n
 ''')
-GooglePlayLink = input('\033[1;32m Insert a Google Play link or Package name: \033[m')
-def PackageName(GooglePlayLink): # For find Package Name from GooglePlay link - like "com.spotify.music"
-    j = 0
-    if GooglePlayLink[0:46] == "https://play.google.com/store/apps/details?id=": # If "GooglePlayLink" is a Link
-        for i in GooglePlayLink:
-            j += 1
-            if i == '=':
-                if GooglePlayLink[-3] == '=':
-                    return (GooglePlayLink[j:-6])
-                elif GooglePlayLink[-9] == '&':
-                    return (GooglePlayLink[j:-9])
-                else:
-                    return (GooglePlayLink[j:])
-    elif ('.' in GooglePlayLink) == True: # If "GooglePlayLink" is a Package name
-        return GooglePlayLink
-    else:
-        print('\n\033[31m [!] Your input not true.\033[m')
-PackageName = PackageName(GooglePlayLink)
 
-def command_function(downloadUrl):
-    if downloadUrl['title'][-1] == 'b':
-        Type = 'Data'
-    elif downloadUrl['title'][-1] == 'k':
-        Type = 'APK'
-    command = input('\n\n\n [*] Download %s File Now?([\033[32mY\033[m]es or [\033[31mN\033[m]o)\033[m: '%Type)
-    if command.lower() == 'y':
-        print('\033[1;33m\n [*] Wait...\033[m')
-        r = requests.get(downloadUrl['href'], allow_redirects=True)
-        open('File/%s' % downloadUrl['title'], 'wb').write(r.content)
-        print('\n\033[32m [*] Downloaded and save in File folder. Enjoy :)\033[m')
-    elif command.lower() == 'n':
-        pass
-    else:
-        print('\n\033[31m [!] Wrong command.\033[n')
-        return command_function()
+try:
+    def details(GP_input):
+        if 'https://' in GP_input or 'http://' in GP_input:
+            pakage_id = BeautifulSoup(requests.get(GP_input).text, 'html.parser').find("meta", attrs={'name': 'appstore:bundle_id'})['content']
+            return pakage_id 
+        elif '.' in GP_input:
+            return GP_input
+        else:
+            print(f'\n{RED} [!] Your input not true.{CLOSE_COLOR}')
+            return None    
+
+    def downloader(downloadURL, name):
+        try:
+            r_downloadURL = requests.get(downloadURL, stream=True)
+
+            with open('File/' + name + '.apk', "wb") as handle:
+                for data in tqdm(r_downloadURL.iter_content()):
+                    handle.write(data)
+            return True
+        except Exception:
+            return False
         
-if PackageName is not None: # If "PackageName" function do not be return "None" value
-    apkdl = 'https://apkdl.in/app/details?id=%s' % PackageName
-    try:
-        r = requests.get(apkdl)
-        App_Page = BeautifulSoup(r.text, 'html.parser')
-        downloadUrl = App_Page.find("a", itemprop='downloadUrl')
-        # For find App/Game info
-        print('\n \033[1;33minfo:\033[m\n  \033[34mName: \033[m'+App_Page.find('b').string)
-        print('  \033[34mVersion: \033[m'+App_Page.find('span', itemprop='softwareVersion').string)
-        print('  \033[34mUpdated on: \033[m'+App_Page.find('span', itemprop='dateModified').string)
-        print('  \033[34mDownload size: \033[m'+App_Page.find('span', itemprop='fileSize').string)
-        # For find App/Game Download Link
-        downloadUrl = ('https://apkdl.in'+downloadUrl['href'])
-        r = requests.get(downloadUrl)
-        DownloadPage = BeautifulSoup(r.text, 'html.parser')
-        downloadUrl = DownloadPage.find_all("a", rel='nofollow')
-        if len(downloadUrl) is 2:
-            command_function(downloadUrl[1])
+    def continue_statement():
+        statement = input(f'{GREEN} [*] Have you any other requests?([Y]es or [N]):\n {YELLOW}> {CLOSE_COLOR}')
+        if statement.lower() == 'yes' or statement.lower() == 'y':
+            return True
+        elif statement.lower() == 'no' or statement.lower() == 'n':
+            return False
+        else:
+            return continue_statement()
 
-        command_function(downloadUrl[0])
+    class Services:
+        @staticmethod
+        def apkdl_in(pakage_id):
+            apkdl = 'https://apkdl.in/app/details?id=%s' % pakage_id
+            r = requests.get(apkdl)
+            App_Page = BeautifulSoup(r.text, 'html.parser')
+            downloadUrl = App_Page.find("a", itemprop='downloadUrl')
 
+            downloadUrl = 'https://apkdl.in'+downloadUrl['href']
+            r = requests.get(downloadUrl)
+            DownloadPage = BeautifulSoup(r.text, 'html.parser')
+            downloadUrl = DownloadPage.find("a", rel='nofollow')
+            return downloadUrl
 
-    except requests.exceptions.ConnectionError:
-        print('\n\033[31m [!] No internet.\n [!] Check your network connection.\033[m')
-    except TypeError:
-        print('\n\033[31m [!] App/Game not found.\033[m')
-    except AttributeError:
-        print('\n\033[31m [!] Your input not true.\033[m')
+        @staticmethod
+        def apkplz_net(pakage_id):
+            url = 'https://apkplz.net/app/%s' % pakage_id
+            r = requests.get(url)
+            App_Page = BeautifulSoup(r.text, 'html.parser')
+            downloadUrl = App_Page.find("div", attrs={'class':'col-sm-12 col-md-12 text-center'})
+
+            downloadUrl = downloadUrl.find("a", rel='nofollow')['href']
+            r = requests.get(downloadUrl)
+            DownloadPage = BeautifulSoup(r.text, 'html.parser')
+            downloadUrl = DownloadPage.find("a", string='click here')
+            return downloadUrl['href']
+
+        @staticmethod
+        def apktada_com(pakage_id):
+            url = 'https://apktada.com/download-apk/%s' % pakage_id
+            r = requests.get(url)
+            App_Page = BeautifulSoup(r.text, 'html.parser')
+            downloadUrl = App_Page.find("a", string='click here')
+            return downloadUrl['href']
+
+        @staticmethod
+        def m_apkpure_com(pakage_id):
+            url = 'https://m.apkpure.com/android/%s/download?from=details' % pakage_id
+            r = requests.get(url)
+            App_Page = BeautifulSoup(r.text, 'html.parser')
+            downloadUrl = App_Page.find("a", string='click here')
+            return downloadUrl['href']
+
+    if __name__ == "__main__":
+        while True:
+            GP_input = input(f'{GREEN} [*] Enter a \"Google Play URL\" or \"APP Code\":\n {YELLOW}> {CLOSE_COLOR}')
+            pakage_id = details(GP_input)
+            service_number = input(f'\n{GREEN} [*] Select one among the following sites:\n 1. apkdl.in\n 2. apkplz.net\n 3. apktada.com\n 4. m.apkpure.com\n {YELLOW}> {CLOSE_COLOR}')
+            if service_number is '1':
+                download_URL = Services.apkdl_in(pakage_id)
+            elif service_number is '2':
+                download_URL = Services.apkplz_net(pakage_id)
+            elif service_number is '3':
+                download_URL = Services.apktada_com(pakage_id)
+            elif service_number is '4':
+                download_URL = Services.m_apkpure_com(pakage_id)
+            else:
+                print(f'\n{RED} [!] The entry is not correct.\n Please choose between one of the top websites!{CLOSE_COLOR}')
+                statement = continue_statement()
+                if statement: continue
+                else: break
+
+            if downloader(download_URL, pakage_id):
+                print(f'\n{GREEN} [*] The download successfully performed and stored in the "file" folder. Enjoy :){CLOSE_COLOR}')
+                statement = continue_statement()
+                if statement: continue
+                else: break
+            else:
+                print(f'\n{GREEN} [!] There is a problem for download.{CLOSE_COLOR}')
+                statement = continue_statement()
+                if statement: continue
+                else: break
+
+except requests.exceptions.ConnectionError:
+    print(f'\n{RED} [!] No Connection.{CLOSE_COLOR}')
+except TypeError:
+    print(f'\n{RED} [!] App/Game not found.\n [!] Try again later.{CLOSE_COLOR}')
+except:
+    print(f'\n{RED} [!] There\'s a problem.\n [!] Try another website.{CLOSE_COLOR}')
